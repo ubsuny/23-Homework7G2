9322601fde104d44d0cd67e2b9d452a3e005bd2b
Yasmin Sarhan <38404107+yasmensarhan27@users.noreply.github.com>
2023-11-29 23:59:21 -0500

    Create NaCl_cubic.py

---
+++ NaCl_cubic.py
@@ -0,0 +1,40 @@
+import Nacl_main.py
+a = 0.236
+
+r_na = np.array( [ [a,0,0], [0, a, 0], [0, 0, a],[a, a, a] ] )
+r_cl = np.array( [ [0, 0, 0],[a, a, 0],[a, 0, a],[0, a, a] ] )
+
+cluster = Cluster(r_na, r_cl)
+vals_init = cluster.get_vals()
+print('initial Na positions:\n', r_na)
+print('initial Cl positions:\n', r_cl)
+print('initial positions flattened shape:\n', vals_init )
+print('initial V  :', cluster.V() )
+
+res = scipy.optimize.minimize( fun=cluster, x0=vals_init, tol=1e-3, method="BFGS")
+cluster.set_vals(res.x)  # For some reason, "minimize" is not updating the class at the last iteration
+print ("Final optimized cluster positions")
+print(cluster.positions)
+print("Final potential:", res.fun)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+charges = cluster.charges
+x,y,z = cluster.positions[:, 0], cluster.positions[:, 1], cluster.positions[:, 2]
+#The yellow dots are for Cl, the green dots are for Na.
+old_na_x= r_na[:,0]
+old_na_y= r_na[:,1]
+old_na_z=r_na[:,2]
+old_cl_x=r_cl[:,0]
+old_cl_y=r_cl[:,1]
+old_cl_z=r_cl[:,2]
+ax.scatter(old_na_x,old_na_y,old_na_z, c='green', label='old Na positions')
+ax.scatter(old_cl_x,old_cl_y,old_cl_z, c='yellow', label='old Cl positions')
+#The blue dots are Cl, the red dots are Na.
+
+ax.scatter( x,y,z, c=charges, cmap='coolwarm', label='Na')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.legend()
+plt.show()

51adb2a9b42e47c3eb75efa67f7641bb6a07728b
Sailesh Bataju <144289804+s4il3sh@users.noreply.github.com>
2023-12-01 11:51:26 -0500

    Update min_max.py

--- min_max.py
+++ min_max.py
@@ -1,3 +1,41 @@
+"""
+This code defines a class for a cluster of Na and Cl atoms and uses 
+scipy.optimize.minimize to find the minimum potential energy configuration of the cluster.
+
+The code imports numpy, matplotlib, mpl_toolkits, itertools, and scipy libraries.
+
+The code defines a function cp(l) that takes a list l and returns a numpy array of 
+all possible pairs of elements from l.
+
+The code defines a class Cluster that takes two numpy arrays of shape (N, 3) 
+for the positions of N Na and N Cl atoms, and initializes the following attributes:
+- positions: a numpy array of shape (2N, 3) for the positions of all atoms in the cluster
+- charges: a numpy array of shape (2N,) for the charges of all atoms in the cluster
+(+1 for Na and -1 for Cl)
+- combs: a numpy array of shape (2N*(2N-1)/2, 2) for all possible pairs of indices of
+atoms in the cluster
+- chargeprods: a numpy array of shape (2N*(2N-1)/2,) for the product of the charges 
+of each pair of atoms in the cluster
+- rij: a numpy array of shape (2N*(2N-1)/2,) for the distance between each pair of 
+atoms in the cluster
+
+The class Cluster also defines the following methods:
+- Vij(): calculates and returns a numpy vector of the potential energy of each pair 
+of atoms in the cluster, using the Coulomb and Lennard-Jones potentials
+- V(): calculates and returns the total potential energy of the cluster, which is the 
+sum of the Vij vector
+- get_vals(): returns the positions of all atoms as a flattened array of shape (6N,)
+- set_vals(vals): takes a flattened array of shape (6N,) and sets the positions of 
+all atoms accordingly, and updates the rij array
+- __call__(vals): takes a flattened array of shape (6N,) and returns the total potential 
+energy of the cluster with the given positions
+The code then creates an instance of the Cluster class with some initial positions of Na and 
+Cl atoms, and prints the initial positions and the initial potential energy.
+The code then uses scipy.optimize.minimize to find the minimum potential energy configuration 
+of the cluster, using the BFGS method and a tolerance of 1e-3. The code prints the final 
+optimized positions and the final potential energy.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D

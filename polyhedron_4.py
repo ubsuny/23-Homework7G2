bd65706b4eed695b8188f2772076a6fdc7adc679
Sailesh Bataju <144289804+s4il3sh@users.noreply.github.com>
2023-12-01 12:08:08 -0500

    Update polyhedron_4.py

--- polyhedron_4.py
+++ polyhedron_4.py
@@ -25,4 +25,27 @@ cluster.set_vals(res.x)  # For some reason, "minimize" is not updating the class
 print ("Final optimized cluster positions")
 print(cluster.positions)
 print("Final potential:", res.fun)
-     
+
+# %matplotlib inline
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+charges = cluster.charges
+x,y,z = cluster.positions[:, 0], cluster.positions[:, 1], cluster.positions[:, 2]
+#The yellow dots are for Cl, the green dots are for Na.
+old_na_x= r_na[:,0]
+old_na_y= r_na[:,1]
+old_na_z=r_na[:,2]
+old_cl_x=r_cl[:,0]
+old_cl_y=r_cl[:,1]
+old_cl_z=r_cl[:,2]
+ax.scatter(old_na_x,old_na_y,old_na_z, c='green', label='old Na positions')
+ax.scatter(old_cl_x,old_cl_y,old_cl_z, c='yellow', label='old Cl positions')
+#The blue dots are Cl, the red dots are Na.
+
+ax.scatter( x,y,z, c=charges, cmap='coolwarm', label='Na')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
+ax.legend()
+plt.show()
